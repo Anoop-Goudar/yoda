38c29413a18b7f6a28aa107f8d5729913907301c
Anoop-Goudar <anoop.a.goudar@gmail.com>
2017-01-21 16:44:53 +0530

    added dashboard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './hotel/login/login.component';
+import { DashboardComponent } from './hotel/dashboard/dashboard.component';
 
 
 
 export const appRoutes: Routes = [
   { path: '', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
   { path: '**', component: AppComponent } // PageNotFoundComponent
 ];
